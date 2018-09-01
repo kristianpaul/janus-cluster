ebc348b892a54a5c0d5e8315ee1f850d0d9c8ae6
Cristian Paul Peñaranda Rojas <paul@kristianpaul.org>
2018-09-01 17:18:59 +0000

    Run with admin api enabled for later service discovery

---
+++ docker/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#Enable admin api
+sed -i "s/admin_http = no/admin_http = yes/g" /opt/janus/etc/janus/janus.transport.http.cfg
+
+#Run with admin api enabled
+/opt/janus/bin/janus  -C /opt/janus/etc/janus/janus.cfg -A
