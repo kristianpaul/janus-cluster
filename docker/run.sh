dbf813c6ccdae191152ab31b9e03034b6db29768
Cristian Paul Penaranda Rojas <paul@kristianpaul.org>
2018-09-30 17:41:54 -0500

    change janus listener to standard http port

--- docker/run.sh
+++ docker/run.sh
@@ -2,6 +2,7 @@
 
 #Enable admin api
 sed -i "s/admin_http = no/admin_http = yes/g" /opt/janus/etc/janus/janus.transport.http.cfg
+sed -i "s/8088/80/g" /opt/janus/etc/janus/janus.transport.http.cfg
 
 #Run with admin api enabled
 /opt/janus/bin/janus  -C /opt/janus/etc/janus/janus.cfg -A
