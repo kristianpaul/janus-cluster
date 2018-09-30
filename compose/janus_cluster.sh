d091d1614215ba3913860134a63e9bf70471acb7
Cristian Paul Penaranda Rojas <paul@kristianpaul.org>
2018-09-30 17:43:14 -0500

    clistering script handles core services as well

--- compose/janus_cluster.sh
+++ compose/janus_cluster.sh
@@ -1,7 +1,11 @@
+export read_only capacity=$(seq 1 3)
+
 if [ "$1" ==  "down" ]; then
-	for i in $(seq 1 3); do export PROJECT=myproj_${i} && docker-compose -p $PROJECT -f docker-compose-janus.yml down; done
+	for i in $capacity; do export PROJECT=myproj_${i} && docker-compose -p $PROJECT -f docker-compose-janus.yml down; done
+	docker-compose -f docker-compose-core.yml down
 fi
 
 if [ "$1" ==  "up" ]; then
-	for i in $(seq 1 3); do export PROJECT=myproj_${i} && docker-compose -p $PROJECT -f docker-compose-janus.yml up -d ; done
+	docker-compose -f docker-compose-core.yml up -d
+	for i in $capacity; do export PROJECT=myproj_${i} && docker-compose -p $PROJECT -f docker-compose-janus.yml up -d ; done
 fi
