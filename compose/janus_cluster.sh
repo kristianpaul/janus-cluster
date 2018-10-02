8b7739dc1bc6f17f2afa621089673b40417d5300
Cristian Paul Penaranda Rojas <paul@kristianpaul.org>
2018-10-01 21:27:36 -0500

    Dumb clustering management

--- compose/janus_cluster.sh
+++ compose/janus_cluster.sh
@@ -1,4 +1,5 @@
 export read_only capacity=$(seq 1 3)
+export read_only max_capacity=$(seq 1 10)
 
 if [ "$1" ==  "down" ]; then
 	for i in $capacity; do export PROJECT=myproj_${i} && docker-compose -p $PROJECT -f docker-compose-janus.yml down; done
@@ -7,5 +8,11 @@ fi
 
 if [ "$1" ==  "up" ]; then
 	docker-compose -f docker-compose-core.yml up -d
+	for i in $capacity; do export PROJECT=myproj_${i} && docker-compose -p $PROJECT -f docker-compose-janus.yml up -d ; done
+fi
+
+if [ "$1" ==  "update" ]; then
+	docker-compose -f docker-compose-core.yml up -d
+	for i in $max_capacity; do export PROJECT=myproj_${i} && docker-compose -p $PROJECT -f docker-compose-janus.yml down; done
 	for i in $capacity; do export PROJECT=myproj_${i} && docker-compose -p $PROJECT -f docker-compose-janus.yml up -d ; done
 fi
