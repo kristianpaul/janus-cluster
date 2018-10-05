bfde0046baa619df3255cc461af38facb2b91410
Cristian Paul Penaranda Rojas <paul@kristianpaul.org>
2018-10-04 22:28:00 -0500

    Prune unused resources

--- compose/janus_cluster.sh
+++ compose/janus_cluster.sh
@@ -4,6 +4,8 @@ export read_only max_capacity=$(seq 1 10)
 if [ "$1" ==  "down" ]; then
 	for i in $capacity; do export PROJECT=myproj_${i} && docker-compose -p $PROJECT -f docker-compose-janus.yml down; done
 	docker-compose -f docker-compose-core.yml down
+	docker network prune -f
+	docker container prune -f
 fi
 
 if [ "$1" ==  "up" ]; then
