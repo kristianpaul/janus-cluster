959a7636c551d4d79cc7685099cee3c6f898b439
Cristian Paul Peñaranda  Rojas <cristian.paul@prexenex.com>
2018-11-13 22:08:34 +0000

    basic janus token management

--- docker/janus/run.sh
+++ docker/janus/run.sh
@@ -7,6 +7,7 @@ sed -i "s/8088/80/g" /opt/janus/etc/janus/janus.transport.http.cfg
 myip=$(curl ifconfig.co/ip)
 sed -i "s/;stun_server/stun_server/g" /opt/janus/etc/janus/janus.cfg
 sed -i "s/;stun_port/stun_port/g" /opt/janus/etc/janus/janus.cfg
+sed -i "s/;rtp_port_range/rtp_port_range/g" /opt/janus/etc/janus/janus.cfg
 sed -i "s/;nat_1_1_mapping = 1.2.3.4/nat_1_1_mapping = $myip/g" /opt/janus/etc/janus/janus.cfg
 
 #Set free RTP port seat
@@ -24,4 +25,8 @@ do
 done
 
 #Run with admin api enabled
-/opt/janus/bin/janus  -C /opt/janus/etc/janus/janus.cfg -A
+screen -S janus -d -m
+screen -r janus -X stuff $'/opt/janus/bin/janus  -C /opt/janus/etc/janus/janus.cfg -A -L /tmp/janus.log\n'
+# Get your self a Token
+http POST localhost:7088/admin/ admin_secret=janusoverlord transaction=FromLocalhost janus=add_token token=mytoken --ignore-stdin
+tail -F /tmp/janus.log
